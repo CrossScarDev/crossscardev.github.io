d9624f40d016cc2f0a255d48fb4e67488c2cdb71
CrossScar <85912189+CommanderGL@users.noreply.github.com>
2023-01-13 21:04:06 -0800

    CSYT 404 Fix

--- CSYT/script.js
+++ CSYT/script.js
@@ -3,5 +3,5 @@ const input = document.querySelector('input')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
-    window.location.href = '/watch#' + input.value.replace('https://www.youtube.com/watch?v=', "")
+    window.location.href = '/CSYT/watch#' + input.value.replace('https://www.youtube.com/watch?v=', "")
 })
